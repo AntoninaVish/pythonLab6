74f64512c4f7b2f0eca0c6461771baa3326432e7
79602 <vishivtonya@gmail.com>
2023-06-12 13:43:19 +0300

    commit

---
+++ lab6.py
@@ -0,0 +1,39 @@
+# a = -6.84
+# print(a * 2 + 3)
+
+# a = -6.84
+# print(abs(a * 2 + 3))
+
+# a = -6.84
+# b = 7
+# c = 25.6
+# print(a, b, c, sep=' | ')
+
+# print('Hello', end=' ')
+# print('World!')
+
+# x = 5.76
+# y = -8
+# print('Координаты точки: х = ', x, '; y = ', y)
+# print(f'Координаты точки: х = {x}; y = {y}')
+
+# a = input()
+# print(a, type(a))
+
+# a = input()
+# a = int(a)
+# print(a, type(a))
+
+
+# a = float(input())
+# print(a)
+
+# a = float(input('Введите длину прямоугольника:'))
+# b = float(input('Введите ширину прямоугольника:'))
+# print('Периметр: ', 2 * (a + b))
+
+# a, b = map(float, input('Введите две стороны прямоугольника: ').split())
+# print('Периметр: ', 2 * (a + b))
+
+a, b, c = map(int, input('Введите стороны треугольника: ').split())
+print('Периметр: ', a + b + c)
